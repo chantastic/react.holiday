0af9584518d1e6c33176e5acf489f822447ab716
Michael Chan <mijoch@gmail.com>
2018-12-19 11:54:34 -0800

    add content back

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,8 +12,8 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content/blog`,
-        name: 'blog',
+        path: `${__dirname}/content`,
+        name: 'content',
       },
     },
     {

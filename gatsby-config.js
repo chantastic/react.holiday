ec46271b629bc1b63df5f93c292214d2c67f137a
Michael Chan <mijoch@gmail.com>
2018-12-19 13:46:34 -0800

    add ga tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-56273983-5`,
       },
     },
     `gatsby-plugin-feed`,
